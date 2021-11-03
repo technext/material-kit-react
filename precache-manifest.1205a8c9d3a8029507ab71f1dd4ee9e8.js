5c5d6ca3b30c6950c9379f0b7b230bbdb039b526
Muazzem Hussen Chowdhury <mhcovi003@gmail.com>
2021-11-03 16:22:29 +0600

    Updates

---
+++ precache-manifest.1205a8c9d3a8029507ab71f1dd4ee9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21535886cece81829fe1d367b448f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea1ec70d051fb9e95171",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "561c84cfc4f3a775cf8e",
+    "url": "/static/css/main.dcea1374.chunk.css"
+  },
+  {
+    "revision": "ea1ec70d051fb9e95171",
+    "url": "/static/js/2.a5e89f40.chunk.js"
+  },
+  {
+    "revision": "561c84cfc4f3a775cf8e",
+    "url": "/static/js/main.2a32ecdc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
